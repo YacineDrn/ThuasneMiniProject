39740d386de458fb45ec4e268895c8aa159bddff
YacineDrn <yac.d.drareni@gmail.com>
2021-01-20 18:49:08 +0100

    first commit

---
+++ src/Firmware.java
@@ -0,0 +1,29 @@
+
+public class Firmware {
+
+	private String firmwareVersion = new String();
+	
+	
+	public Firmware(String version)
+	{
+		this.firmwareVersion=version;
+	}
+
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	public String getFirmwareVersion() {
+		return firmwareVersion;
+	}
+
+	public void setFirmwareVersion(String firmwareVersion) {
+		this.firmwareVersion = firmwareVersion;
+	}
+
+
+
+
+}
+
+
